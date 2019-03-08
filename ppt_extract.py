6941f84a3aad2a5f8c731b48cf01a960dac19dfb
GoldenGrape <goldengrape@goldengrape.org>
2019-03-09 02:22:00 +0800

    导出静态画面的配音完成

--- ppt_extract.py
+++ ppt_extract.py
@@ -3,11 +3,14 @@
 
 # 读取ppt, 将ppt中的注释文字提取出来
 
-# In[11]:
+# In[1]:
 
 
 from pptx import Presentation
-import pandas as pd
+from generate_voice import tts
+import os
+import re
+import subprocess
 
 
 # In[2]:
@@ -17,7 +20,7 @@ if __name__=="__main__":
     ppt_filename="data/test.pptx"
 
 
-# In[22]:
+# In[3]:
 
 
 def get_notes_text(ppt_filename):
@@ -32,13 +35,7 @@ def get_notes_text(ppt_filename):
 
 # 将每个注释转换成语音
 
-# In[19]:
-
-
-from generate_voice import tts
-
-
-# In[27]:
+# In[4]:
 
 
 def get_notes_voice(notes_dict):
@@ -50,7 +47,7 @@ def get_notes_voice(notes_dict):
 
 # 将语音分别保存为音频mp3
 
-# In[ ]:
+# In[5]:
 
 
 if __name__=="__main__":
@@ -59,19 +56,74 @@ if __name__=="__main__":
     voice_dict=get_notes_voice(notes_dict)
 
 
-# In[33]:
+# In[6]:
 
 
-def save_notes_voice(voice_dict):
+def save_notes_voice(voice_dict,images_path):
     temp_file_dict={}
     for index, voice_data in voice_dict.items():
-        temp_filename="data/temp_{:03d}.mp3".format(index)
+        temp_filename=os.path.join(images_path,"temp_{:03d}.mp3".format(index))
         with open(temp_filename, 'wb') as outfile:
             outfile.write(voice_data)
+#             print("save ", temp_filename)
         temp_file_dict[index]=temp_filename
     return temp_file_dict
 
 
-# 将音频mp3插入到每张幻灯片中, 
-# 
-# 咦, 似乎卡关了. 没有找到如何将mp3插入到幻灯中的方法
+# 将每页导出成图像, 然后把每张图像与每个音频做成一个视频
+
+# In[7]:
+
+
+def build_slide_video(images_path):
+    image_filename_list = [f for f in os.listdir(images_path) if f.endswith('.png')]
+    audio_filename_list = save_notes_voice(voice_dict,images_path)
+    video_filename_list = ["slide_{:03d}.mp4".format(index)
+                           for index in range(len(image_filename_list))]
+    for filename in image_filename_list:
+        (prefix, index_str,ends)=re.findall("([\u4e00-\u9fa5A-Z]+)(\d+)(.png)",filename)[0]
+        index=int(index_str)-1 # 注意python从0开始, ppt导出图片从1开始
+
+        image_filename=os.path.join(images_path,filename)
+        audio_filename=audio_filename_list[index]
+        video_filename=os.path.join(images_path,video_filename_list[index])
+
+        ffmpeg_str="ffmpeg -loop 1 -i {} -i {} -c:v libx264 -c:a copy -shortest {}".format(
+            image_filename,audio_filename,video_filename
+        )
+        subprocess.call(["ffmpeg", "-loop", "1", 
+                         "-i", image_filename,
+                         "-i", audio_filename,
+                         "-c:v", "libx264", "-c:a", "copy", "-shortest",
+                         video_filename])
+#         print("save ", video_filename )
+    # 记录产生的文件名
+    video_list=os.path.join(images_path,"video_file_list.txt")
+    with open(video_list, "w") as f:
+        for v in video_filename_list:
+            f.write("file "+"'{}'\n".format(v)) # 注意ffmpeg在从txt里调用文件名时, 似乎是用了相对路径
+    
+    return video_list
+
+
+# 把所有视频连起来
+
+# In[8]:
+
+
+def generate_slides_with_voice(ppt_filename):
+    images_path=ppt_filename.split(".")[0]
+    video_list=build_slide_video(images_path)
+    # ffmpeg -f concat -i mylist.txt -c copy output
+    subprocess.call(["ffmpeg", "-f", "concat",
+                     "-i", video_list,
+                     "-c", "copy",
+                     ppt_filename+".mp4"])
+
+
+# In[9]:
+
+
+if __name__=="__main__":
+    generate_slides_with_voice(ppt_filename)
+
